4191687353980e0e91625bbf432a6ad4e3731624
Yann Labou <Yann.Labou@gmail.com>
2017-10-14 14:17:57 +0200

    New features
    - std::string_view
    - Initializer in if statements
    - Filesystem

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,37 @@
+// Name:   Yann Labou
+// E-Mail: Yann.Labou@gmail.com
+
+// Cpp17 Features
+// - std::string_view
+// - Initializer in 'if' and 'switch'
+// - Filesystem
+
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <algorithm>
 #include <vector>
-#include <iterator>
+#include <experimental/iterator> //http://en.cppreference.com/w/cpp/experimental/ostream_joiner
+#include <experimental/filesystem>
 
-bool is_empty(std::ifstream& input)
-{
-    if (input)
-        return input.peek() == std::ifstream::traits_type::eof();
-    return true;
-}
+namespace fs = std::experimental::filesystem;
 
-std::vector<std::string> split(std::string_view csv_row, char delimiter = ',')
+template<typename C>
+std::vector<std::basic_string<C>> split(const std::basic_string<C>& text, C delimiter = ',')
 {
-    std::vector<std::string> result;
-    auto p = std::begin(csv_row);
-    for (auto q = std::find(p, std::end(csv_row), delimiter);
-         q != std::end(csv_row);
-         q = std::find(++p, std::end(csv_row), delimiter))
+    using bs_t = std::basic_string<C>;
+    std::vector<bs_t> rv;
+    auto p = std::begin(text);
+    for (auto q = std::find(p, std::end(text), delimiter);
+         q != std::end(text);
+         q = std::find(++p, std::end(text), delimiter))
     {
-        result.emplace_back(p, q);
+        rv.emplace_back(p, q);
         p = q;
     }
-    if (p != std::end(csv_row))
-        result.emplace_back(p, std::end(csv_row));
-    return result;
+    if (p != std::end(text))
+        rv.emplace_back(p, std::end(text));
+    return rv;
 }
 
 template<typename Container>
@@ -33,7 +40,8 @@ void write_csv_row(Container first, Container last,
                    std::string_view delimiter,
                    size_t line_number)
 {
-    std::ofstream ofs{filename.data(), std::ios::out | (line_number ? std::ios::app : std::ios::trunc) | std::ios::binary};
+    std::ofstream ofs{filename.data(),
+                      std::ios::out | (line_number ? std::ios::app : std::ios::trunc) | std::ios::binary};
     std::copy(first, last - 1, std::ostream_iterator<std::string>(ofs, delimiter.data()));
     std::copy(last - 1, last, std::ostream_iterator<std::string>(ofs, "\n"));
     ofs.close();
@@ -43,52 +51,54 @@ int main(int argc, char* argv[])
 {
     if (argc != 5)
     {
-        std::cerr << "Invalid number of arguments!" << '\n';
-        return -1;
-    }
-
-    std::vector<std::string> args;
-    args.reserve(4);
-    for (int i = 1; i != argc; ++i)
-        args.emplace_back(argv[i]);
-
-    std::string csv_input{args.at(0)};
-    std::string column_name{args.at(1)};
-    std::string replacement_text{args.at(2)};
-    std::string csv_output{args.at(3)};
-
-
-    std::ifstream ifs{csv_input};
-    if (is_empty(ifs))
+        std::cout << "Invalid number of arguments!" << '\n';
+        std::vector<std::string> args{"Usage:", argv[0], "<csv_filename>",
+                "<column_override>", "<text_replacement>", "<output_file>"};
+        std::copy(std::begin(args),
+                  std::end(args),
+                  std::experimental::make_ostream_joiner(std::cout, " "));
+        return EXIT_FAILURE;
+    } else if (fs::path const& csv_input{argv[1]}; !fs::exists(csv_input))
     {
-        std::cerr << "input file missing" << '\n';
-        return -1;
+        std::cout << "input file not existing on file system" << '\n';
+        return EXIT_FAILURE;
+    } else if (!fs::file_size(csv_input))
+    {
+        std::cout << "input file missing" << '\n';
+        return EXIT_FAILURE;
     }
 
-    std::string str_csv_row;
-    std::vector<std::string> vec_csv_row;
+    std::string_view csv_input{argv[1]};
+    std::string_view column_name{argv[2]};
+    std::string_view replacement_text{argv[3]};
+    std::string_view csv_output{argv[4]};
+
+    std::string csv_line;
+    std::vector<std::string> csv_fields;
     std::vector<std::string>::iterator city_column;
     std::ptrdiff_t index;
     std::size_t line_number = 0;
-    while(std::getline(ifs, str_csv_row))
+
+    std::ifstream ifs{csv_input.data()};
+    while(std::getline(ifs, csv_line))
     {
-        vec_csv_row = split(str_csv_row);
+        csv_fields = split(csv_line);
         if (!line_number)
         {
-            city_column = std::find(vec_csv_row.begin(), vec_csv_row.end(), "City");
-            if (!(city_column != vec_csv_row.end()))
+            city_column = std::find(csv_fields.begin(), csv_fields.end(), column_name);
+            if (!(city_column != csv_fields.end()))
             {
                 std::cerr << "column name doesn't exists in the input file" << '\n';
-                break;
+                return EXIT_FAILURE;
             }
-            index = std::distance(vec_csv_row.begin(), city_column);
+            index = std::distance(csv_fields.begin(), city_column);
         }
         else
         {
-            vec_csv_row.at(index) = replacement_text;
+            csv_fields.at(index) = replacement_text;
         }
-        write_csv_row(vec_csv_row.begin(), vec_csv_row.end(), csv_output, ",", line_number);
+        write_csv_row(csv_fields.begin(), csv_fields.end(), csv_output, ",", line_number);
         ++line_number;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
